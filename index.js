b7ee39bac091d368f3879000b11cd4d8d4cf69a6
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2020-02-21 22:26:29 -0800

    fix the port

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express');
 const server = express();
 const postsRoutes = require('./data/appRoutes/posts.js');
 var cors = require('cors');
-server.use(express.json())
-require("dotenv").config()
+server.use(express.json());
+require('dotenv').config();
 
-
+const port = process.env.PORT || 5000;
 
 server.use(cors());
-server.use('/',(req,res)=>{res.send(`<h1>${process.env.WELCOME}</h1>`)})
+server.use('/', (req, res) => {
+	res.send(`<h1>${process.env.WELCOME}</h1>`);
+});
 server.use('/api/', postsRoutes);
 
+server.use((req, res) => {
+	res.status(404).send('we will fix it soon');
+});
 
-server.use((req,res)=>{
-	res.status(404).send('we will fix it soon')
-})
-
-
-server.listen(5000, () => {
-	console.log('API is working on port 5000');
+server.listen(port, () => {
+	console.log(`API is working on port ${port}`);
 });
